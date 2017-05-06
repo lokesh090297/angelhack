c8e3762fd23c5846df294f8fe31751d7904d055f
aayushi2601 <aayushi.garg@tothenew.com>
2017-05-06 17:37:43 +0530

    GymForm

--- modules/custom/member_payment/src/Form/GymForm.php
+++ modules/custom/member_payment/src/Form/GymForm.php
@@ -7,30 +7,42 @@ use Drupal\Core\Form\FormStateInterface;
 use \Drupal\node\Entity\Node;
 use \Drupal\file\Entity\File;
 use Drupal\user\Entity\User;
-class PaymentForm extends FormBase
+class GymForm extends FormBase
 {
   public function getFormId() 
 {
-    return 'payment_add';
+    return 'gym_add';
 }
   public function buildForm(array $form, FormStateInterface $form_state) 
 {
       
          $form['user_id'] = array(
-      		'#type' => 'hidden',
+      	'#type'  => 'textfield',
+        '#title' => t('User ID'),
+        '#required' => TRUE,
        	);
-         $form['payment_amount'] = array(
+         $form['hours'] = array(
          '#type'  => 'textfield',
-         '#title' => t('Add amount to your wallet'),
+         '#title' => t('No. of hours'),
          '#required' => TRUE,
-      );    
+      ); 
+         $form['co-members'] = array(
+         '#type'  => 'textfield',
+         '#title' => t('Co-Gymmembers'),
+         '#required' => TRUE,
+      ); 
+          $form['amount'] = array(
+         '#type'  => 'textfield',
+         '#title' => t('Amount'),
+         '#required' => TRUE,
+      ); 
        $form['actions']['#type']= 'actions';
      
-     $form['actions']['submit'] = array(
+       $form['actions']['submit'] = array(
           '#type'  => 'submit',
           '#value' => t('Add'), 
-    );
-    return $form;
+       );
+       return $form;
 }
   public function validateForm(array &$form, FormStateInterface $form_state) 
   { 
@@ -38,19 +50,24 @@ class PaymentForm extends FormBase
   }
   public function submitForm(array &$form, FormStateInterface $form_state) 
   {  
-    $current_user = \Drupal::currentUser();
-    $id = $current_user->id();
-    $payment_amount = $form_state->getValue('payment_amount');
-    $id = 2;
+    $id = $form_state->getValue('user_id');
+    $payment_amount = $form_state->getValue('amount');
     $result = db_query("Select payment_amount from member_payment where user_id = '" . $id . "'")->fetchCol();
     if(isset($result[0]))
-    {
-     $upd_amt = $result[0]-$payment_amount;  
-      db_query("Update member_payment set payment_amount='$upd_amt' where user_id='$id'");
+    { 
+       if($result[0] == 0)
+       {
+          echo 'asds';
+          die;
+       }
+       else {
+         echo 'sdsd';
+         die;
+       }
     }
    else 
     {
-      db_query("insert into member_payment values ('$id','$payment_amount')" );
+      
     }
     
 }
