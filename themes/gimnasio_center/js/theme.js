620d9a2f4ed4011a0f8b7f25f2359f4caa55fc0a
Deepali_agrwl <agarwaldeepali33@gmail.com>
2017-05-07 11:09:19 +0530

    Design changes

--- themes/gimnasio_center/js/theme.js
+++ themes/gimnasio_center/js/theme.js
@@ -3,6 +3,9 @@
     attach: function (context) {
       var height = jQuery('.path-frontpage #navbar').outerHeight();
       jQuery('.path-frontpage .main-container').css('margin-top', height);
+      var minHeight = jQuery(window).height();
+      jQuery('.main-container').css('min-height', minHeight - 150);
+      jQuery('.path-frontpage .main-container').css('min-height', 0);
     }
   };
 })(jQuery);
